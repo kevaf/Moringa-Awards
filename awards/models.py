1a97cee0592216a358db912997d43a10b263f9c6
kevaf <kevinkipyegon20@gmail.com>
2021-10-23 13:12:04 +0300

    added comments model class

--- awards/models.py
+++ awards/models.py
@@ -53,3 +53,23 @@ class Ratings(models.Model):
     content = models.IntegerField(default=1)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     project_id = models.ForeignKey(Projects,on_delete=models.CASCADE)
+
+class Comments(models.Model):
+    project_id = models.ForeignKey(Projects,on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+
+    @classmethod
+    def get_all_comments(cls,id):
+        comments = cls.objects.filter(project_id = id)
+        return comments
+
+    def save_comments(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
