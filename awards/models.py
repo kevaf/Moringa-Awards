c9b4b6073bcd273bb075c7cd5b65861700103050
kevaf <kevinkipyegon20@gmail.com>
2021-10-23 13:06:36 +0300

    modified profile class

--- awards/models.py
+++ awards/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete = models.CASCADE)
-    dpic = models.ImageField(upload_to = 'images/')
+    profpic = models.ImageField(upload_to = 'images/')
     bio = models.TextField(max_length=1000)
-    info = models.TextField(max_length=5000)
+    about = models.TextField(max_length=5000)
+
+
+    def __str__(self):
+        return self.username
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
