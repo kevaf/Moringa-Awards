85d1dbfe85bd0f3ff4ed5df65a2c6e81547b1d33
kevaf <kevinkipyegon20@gmail.com>
2021-10-24 12:24:53 +0300

    modified views

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Profile, Projects, Comments, Ratings
+from django.contrib.auth.models import User
 # Create your views here.
 
 @login_required(login_url = '/accounts/login/')
 def index(request):
+    all_projects = Projects.all_projects()
     
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'all_projects': all_projects})
